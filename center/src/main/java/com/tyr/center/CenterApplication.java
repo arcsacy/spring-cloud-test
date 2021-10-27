7c6b669ec9cc6eec3c951b4f77bad673676632c9
arcsacy <arcsacy@163.com>
2021-10-27 10:07:46 +0800

    SpringCloud  Test v1.0.0  2021/10/27   添加配置中心服务，可自动刷新配置文件状态

--- center/src/main/java/com/tyr/center/CenterApplication.java
+++ center/src/main/java/com/tyr/center/CenterApplication.java
@@ -2,12 +2,10 @@ package com.tyr.center;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.config.server.EnableConfigServer;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@EnableEurekaServer
-@EnableConfigServer
 @SpringBootApplication
+@EnableEurekaServer
 public class CenterApplication {
 
     public static void main(String[] args) {
